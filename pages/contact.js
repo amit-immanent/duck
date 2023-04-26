9ca4fe98bb05e5644943b1981ebf54c37820b8a6
amit-immanent <amityadav.immanent@gmail.com>
2023-04-26 12:34:18 +0530

    add + in contact us page

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,7 @@ import Header from '@/components/Header'
 const contact = () => {
   return (<>
     <Header/>
-    <div style={{textAlign: "center", marginTop: "30px"}}>Contact Page</div>
+    <div style={{textAlign: "center", marginTop: "30px"}}>Contact Page +</div>
   </>
   )
 }
